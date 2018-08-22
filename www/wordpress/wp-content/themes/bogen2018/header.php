a5d1489c83fca5ced9fccf9280e296e409971c52
Yusuke Okada <demi168@YusukenoMacBook-Pro.local>
2018-08-22 19:24:14 +0900

    公開前調整

--- www/wordpress/wp-content/themes/bogen2018/header.php
+++ www/wordpress/wp-content/themes/bogen2018/header.php
@@ -16,6 +16,14 @@
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.backstretch.min.js"></script>
+	<!-- Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-3699864-20');
+	</script>
 </head>
 <body <?php body_class(); ?>>
 	<div id="wptime-plugin-preloader"></div><!-- Loading -->
--- www/wordpress/wp-content/themes/bogen2018/template-parts/content-none.php
+++ www/wordpress/wp-content/themes/bogen2018/template-parts/content-none.php
@@ -1,9 +1,9 @@
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title">準備中です</h1>
+		<h1 class="page-title">該当カテゴリーに記事がありません。</h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<p>月1回更新のコラムがはじまります。今書いていただいているので、もうしばらくお待ちくださいませ。</p>
+		<p>現在準備中です。いましばらくお待ちくださいませ。</p>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
