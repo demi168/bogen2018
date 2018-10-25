fe84f63cf869f2b8454db411e15b8bb6516239d4
Yusuke Okada <demi168@YusukenoMacBook-Pro.local>
2018-10-25 11:45:27 +0900

    調整

--- www/wordpress/wp-content/themes/bogen2018/single.php
+++ www/wordpress/wp-content/themes/bogen2018/single.php
@@ -16,53 +16,54 @@
 					get_template_part( 'template-parts/content', get_post_type() );
 				endwhile; // End of the loop.
 				?>
+
+				<div class="article__list">
+					<?php
+						$post_id = get_the_ID();
+						foreach((get_the_category()) as $cat) {
+							$cat_id = $cat->cat_ID ;
+							break ;
+						}
+						query_posts(
+							array(
+								'cat' => $cat_id,
+								'showposts' => 8,
+								'post__not_in' => array($post_id)
+							)
+						);
+						if(have_posts()) :
+					?>
+					<?php while (have_posts()) : the_post(); ?>
+					<article class="article__list_unit blog">
+						<div class="article__list_unit_thumb">
+							<a href="<?php the_permalink(); ?>">
+								<?php if (has_post_thumbnail()) : ?>
+									<?php the_post_thumbnail('thumbnail'); ?>
+								<?php else : ?>
+									<img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>" />
+								<?php endif ; ?>
+							</a>
+						</div>
+						<header>
+							<h1 class="article__list_unit_title">
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</h1>
+							<p class="article__list_unit_info">
+								<span><?php the_time('Y.m.d'); ?></span>
+								<span>|</span>
+								<span>
+									<?php echo get_the_category_list( ' ,' ); ?>
+								</span>
+							</p>
+						</header>
+					</article>
+					<?php endwhile; ?>
+					<?php endif; ?>
+					<?php wp_reset_query(); ?>
+				</div><!-- /.article__list -->
+				
 			</div>
 		</section>
-
-		<div class="article__list">
-			<?php
-				$post_id = get_the_ID();
-				foreach((get_the_category()) as $cat) {
-					$cat_id = $cat->cat_ID ;
-					break ;
-				}
-				query_posts(
-					array(
-						'cat' => $cat_id,
-						'showposts' => 8,
-						'post__not_in' => array($post_id)
-					)
-				);
-				if(have_posts()) :
-			?>
-			<?php while (have_posts()) : the_post(); ?>
-			<article class="article__list_unit blog">
-				<div class="article__list_unit_thumb">
-					<a href="<?php the_permalink(); ?>">
-						<?php if (has_post_thumbnail()) : ?>
-							<?php the_post_thumbnail('thumbnail'); ?>
-						<?php else : ?>
-							<img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>" />
-						<?php endif ; ?>
-					</a>
-				</div>
-				<header>
-					<h1 class="article__list_unit_title">
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</h1>
-					<p class="article__list_unit_info">
-						<span><?php the_time('Y.m.d'); ?></span>
-						<span>|</span>
-						<span>
-							<?php echo get_the_category_list( ' ,' ); ?>
-						</span>
-					</p>
-				</header>
-			</article>
-			<?php endwhile; ?>
-			<?php endif; ?>
-			<?php wp_reset_query(); ?>
-		</div>
 	</div><!-- /.content__container -->
 
 <?php get_footer(); ?>
